08ca98620b976ec00d75471b59bee6197e020d6a
puruscor <purus.cor@gmail.com>
2021-04-14 21:08:48 +0300

    Fix gob get horse

--- src/haven/purus/pbot/api/PBotGobAPI.java
+++ src/haven/purus/pbot/api/PBotGobAPI.java
@@ -70,7 +70,7 @@ public class PBotGobAPI {
 						break;
 					} catch(Loading l){PBotUtils.sleep(10);}
 				}
-				if(res != null && res.name.endsWith("/horse") && new Coord2d(gob.getc()).dist(getPlayer().getCoords()) == 0)
+				if(res != null && res.name.startsWith("gfx/kritter/horse/") && new Coord2d(gob.getc()).dist(getPlayer().getCoords()) == 0)
 					continue;
 				PBotGob pgob = new PBotGob(gob, pBotSession);
 					if(res != null && pat.matcher(res.name).matches() && pgob.dist(getPlayer()) < bestDistance) {
