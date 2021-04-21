431e4a75111468732fd804d1587dd79758fccba1
puruscor <purus.cor@gmail.com>
2021-04-21 17:54:56 +0300

    Tick inactive session ui

--- src/haven/JOGLPanel.java
+++ src/haven/JOGLPanel.java
@@ -531,6 +531,7 @@ public class JOGLPanel extends GLCanvas implements Runnable, UIPanel, Console.Di
 					if(session.sess != null && session != MultiSession.activeSession) {
 						synchronized(session) {
 							session.sess.glob.ctick();
+							session.sess.ui.tick();
 						}
 					}
 				}
