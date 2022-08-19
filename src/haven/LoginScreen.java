6628f8c8ee06a64b0deaac42e086cd97b249babc
puruscor <purus.cor@gmail.com>
2022-08-19 16:50:12 +0300

    Modify loginscreen

--- src/haven/LoginScreen.java
+++ src/haven/LoginScreen.java
@@ -44,7 +44,7 @@ public class LoginScreen extends Widget {
     public static final Text.Foundry
 	textf = new Text.Foundry(Text.sans, 16).aa(true),
 	textfs = new Text.Foundry(Text.sans, 14).aa(true);
-    public static final Tex bg = Resource.loadtex("gfx/loginscr");
+    public static final Tex bg = Resource.loadtex("gfx/loginscr2");
     public static final Position bgc = new Position(UI.scale(420, 300));
     public final Credbox login;
     public final String hostname;
